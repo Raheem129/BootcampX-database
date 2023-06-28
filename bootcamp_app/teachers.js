d2249d6208c2ad90e57d3f912724ec72053d8843
Abdi-Raheem <raheem129@hotmail.com>
2023-06-28 13:57:38 -0400

    updated js files to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    console.log('connected');
+    res.rows.forEach(row => {
+      console.log(`ID: ${row.student_id}, Name: ${row.name}, Cohort: ${row.cohort}`);
+    });
   })
-});
+  .catch(err => console.error('query error', err.stack))
